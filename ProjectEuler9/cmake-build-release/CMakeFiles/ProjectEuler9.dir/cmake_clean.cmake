a57e463cd005fd7d7603d4054833c69c17e292d5
attila <daf276@gmx.de>
2018-05-04 12:24:58 +0200

    Problem 8 and 10 not solved

---
+++ ProjectEuler9/cmake-build-release/CMakeFiles/ProjectEuler9.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectEuler9.dir/main.cpp.o"
+  "ProjectEuler9.pdb"
+  "ProjectEuler9"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectEuler9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ProjectEuler9/main.cpp
+++ ProjectEuler9/main.cpp
@@ -1,14 +1,35 @@
 #include <iostream>
+#include <chrono>
+#include <ctime>
+#include <cmath>
 
-int main(int argc, char const *argv[])
-{
-	int sum = 0;
+int main() {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
 
-	for (int i = 3; i < 1000; ++i)
-	{
-		if(i % 3 == 0 || i % 5 == 0) sum += i;
-	}
+    int y = 1000;
+    int a, b, c;
+    bool solutionFound = false;
 
-	std::cout << sum << std::endl;
-	return 0;
+    for (a = 1; a < y/3; a++) {
+        for (b = 1; b < y/2; b++) {
+            double potentialC = sqrt(pow(a, 2) + pow(b, 2));
+            if ((int) potentialC == potentialC) {
+                c = (int) potentialC;
+                if (a + b + c == y) {
+                    solutionFound = true;
+                    break;
+                }
+            }
+        }
+        if (solutionFound) break;
+    }
+
+    end = std::chrono::system_clock::now();
+    long elapsed_millis = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "Elapsed Milliseconds: " << elapsed_millis << std::endl;
+
+    std::cout << "a: " << a << " b: " << b << " c: " << c << std::endl;
+    std::cout << a * b * c << std::endl;
+    return 0;
 }
