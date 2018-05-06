5334058badc43661300db807f965b12898539601
attila <daf276@gmx.de>
2018-05-06 21:03:56 +0200

    Problem 9 and 10

---
+++ ProjectEuler10/cmake-build-release/CMakeFiles/ProjectEuler10.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjectEuler10.dir/main.cpp.o"
+  "ProjectEuler10.pdb"
+  "ProjectEuler10"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjectEuler10.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- ProjectEuler10/main.cpp
+++ ProjectEuler10/main.cpp
@@ -1,14 +1,55 @@
 #include <iostream>
+#include <chrono>
+#include <cmath>
+#include <vector>
 
-int main(int argc, char const *argv[])
-{
-	int sum = 0;
+int main() {
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
 
-	for (int i = 3; i < 1000; ++i)
-	{
-		if(i % 3 == 0 || i % 5 == 0) sum += i;
-	}
+    std::vector<int> primes = {2, 3};
+    long primeSum = 5;
 
-	std::cout << sum << std::endl;
-	return 0;
+    int potentialPrime1, potentialPrime2;
+    bool number1isPrime, number2isPrime;
+
+    for (int i = 6; i < 2000000; i += 6) {
+        potentialPrime1 = i - 1;
+        potentialPrime2 = i + 1;
+
+        number1isPrime = true;
+        number2isPrime = true;
+
+        for (int number : primes) {
+            if (number > sqrt(potentialPrime1)) break;
+            else if (potentialPrime1 % number == 0) {
+                number1isPrime = false;
+                break;
+            }
+        }
+
+        for (int number : primes) {
+            if (number > sqrt(potentialPrime2)) break;
+            else if (potentialPrime2 % number == 0) {
+                number2isPrime = false;
+                break;
+            }
+        }
+
+        if (number1isPrime) {
+            primes.push_back(potentialPrime1);
+            primeSum += potentialPrime1;
+        }
+        if (number2isPrime) {
+            primes.push_back(potentialPrime2);
+            primeSum += potentialPrime2;
+        }
+    }
+
+    end = std::chrono::system_clock::now();
+    long elapsed_millis = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
+    std::cout << "Elapsed Milliseconds: " << elapsed_millis << std::endl;
+
+    std::cout << primeSum << std::endl;
+    return 0;
 }
