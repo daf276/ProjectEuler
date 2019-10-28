b947ed17983d6598016512a862b3c0ab11b114fa
Attila <daf276@gmx.de>
2019-10-28 07:39:36 +0100

    did 8 in rust

---
+++ ProjectEuler8/target/debug/ProjectEuler8.d
@@ -0,0 +1,1 @@
+C:\Users\Attila\IdeaProjects\ProjectEuler\ProjectEuler8\target\debug\ProjectEuler8.exe: C:\Users\Attila\IdeaProjects\ProjectEuler\ProjectEuler8\src\main.rs
---
+++ ProjectEuler8/target/release/ProjectEuler8.d
@@ -0,0 +1,1 @@
+C:\Users\Attila\IdeaProjects\ProjectEuler\ProjectEuler8\target\release\ProjectEuler8.exe: C:\Users\Attila\IdeaProjects\ProjectEuler\ProjectEuler8\src\main.rs
