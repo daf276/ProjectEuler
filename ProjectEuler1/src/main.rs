0164e967153e3f21422a9f97efffcb87a2a3a99d
Attila <daf276@gmx.de>
2019-10-31 12:20:56 +0100

    Added gitignore

--- ProjectEuler1/src/main.rs
+++ ProjectEuler1/src/main.rs
@@ -2,13 +2,7 @@ use std::time::{Duration, Instant};
 
 fn main() {
     let now = Instant::now();
-    let mut sum = 0;
-
-    for i in 3..1000 {
-        if i % 3 == 0 || i % 5 == 0 {
-            sum += i;
-        }
-    }
+    let mut sum:i32 = (3..1000).filter(|&x| x % 3 == 0 || x % 5 == 0).sum();
 
     println!("Sum: {}", sum);
     println!("Elapsed µs: {}", now.elapsed().as_micros());
--- ProjectEuler12/src/main.rs
+++ ProjectEuler12/src/main.rs
@@ -1,30 +1,51 @@
 extern crate primal;
+
 use std::time::Instant;
 
 const NUM_OF_DIVISORS: u64 = 500;
 
+struct Triangle {
+    curr: usize,
+    next_summand: usize,
+}
+
+impl Iterator for Triangle {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<usize> {
+        self.curr = self.curr + self.next_summand;
+        self.next_summand += 1;
+
+        Some(self.curr)
+    }
+}
+
+fn triangle() -> Triangle {
+    Triangle {
+        curr: 1,
+        next_summand: 2,
+    }
+}
+
 fn main() {
-
     let now = Instant::now();
-
     let sieve = primal::Sieve::new(NUM_OF_DIVISORS.pow(2) as usize);
 
-    let mut triangle_number = 1;
-    let mut next_summand = 2;
-    let mut number_found = false;
+    let result = triangle()
+        .map(|triangle_num| {
+            let product = sieve
+                .factor(triangle_num)
+                .unwrap()
+                .iter()
+                .map(|x| (x.1 as u64) + 1)
+                .fold(1, |mut product, x| product * x);
+            (product, triangle_num)
+        })
+        .filter(|pair| pair.0 > NUM_OF_DIVISORS)
+        .next()
+        .unwrap()
+        .1;
 
-    while !number_found {
-        let cnt:u64 = sieve.factor(triangle_number).unwrap().iter()
-            .map(|x| (x.1 as u64) + 1).fold(1, |mut product, x| {product *= x; product});
-
-        if cnt > NUM_OF_DIVISORS {
-            number_found = true;
-        } else {
-            triangle_number += next_summand;
-            next_summand += 1;
-        }
-    }
-
-    println!("Triangle number: {}", triangle_number);
+    println!("Triangle number: {}", result);
     println!("Elapsed µs: {}", now.elapsed().as_micros());
 }
